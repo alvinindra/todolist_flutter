9d2a42cca9d394a5827a4cb6318536eb42be1d30
unknown <alvinindrap@gmail.com>
2020-10-18 14:53:08 +0700

    feat: create access database sqflite

---
+++ lib/access_database.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class AccessDatabase {
+  void _createDb(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE todo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        desc TEXT,
+        date TEXT
+      )
+    ''');
+  }
+
+  Future<Database> initDb() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'todo.db';
+    var todoDatabase = openDatabase(path, version: 1, onCreate: _createDb);
+    return todoDatabase;
+  }
+}
