33bce61da03cf3b827d90b02ee96db8d0e08e630
unknown <alvinindrap@gmail.com>
2020-10-18 14:45:09 +0700

    feat: add class catcher for map

---
+++ lib/class_catcher.dart
@@ -0,0 +1,33 @@
+class ClassCatcher {
+  int _id;
+  String _title;
+  String _desc;
+
+  ClassCatcher(this._title, this._desc);
+
+  ClassCatcher.fromMap(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._desc = map['desc'];
+  }
+
+  int get id => _id;
+  String get title => _title;
+  String get desc => _desc;
+
+  set title(String value) {
+    _title = value;
+  }
+
+  set desc(String value) {
+    _desc = value;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = id;
+    map['title'] = title;
+    map['desc'] = desc;
+    return map;
+  }
+}
