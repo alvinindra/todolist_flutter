daa6b68abd8e739e096f54d4686d84d322c36ff1
Julio Rahman <julio.rahman@gmail.com>
2020-10-19 20:44:15 +0700

    style: update enter layout

--- lib/enter.dart
+++ lib/enter.dart
@@ -31,81 +31,48 @@ class _EntryFormState extends State<EntryForm> {
           ),
         ),
         body: Padding(
-          padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+          padding: EdgeInsets.all(16.0),
           child: ListView(
             children: <Widget>[
-              // nama
               Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: TextField(
+                padding: EdgeInsets.only(bottom: 16.0),
+                child: TextFormField(
                   controller: titleController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                     labelText: 'Judul',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+                    border: OutlineInputBorder(),
                   ),
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: TextField(
+                padding: EdgeInsets.only(bottom: 16.0),
+                child: TextFormField(
                   controller: descController,
-                  keyboardType: TextInputType.text,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
                   decoration: InputDecoration(
                     labelText: 'Deskripsi',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        child: Text(
-                          'Save',
-                          textScaleFactor: 1.5,
-                        ),
-                        onPressed: () {
-                          if (todo == null) {
-                            todo = ClassCatcher(
-                                titleController.text, descController.text);
-                          } else {
-                            todo.title = titleController.text;
-                            todo.desc = descController.text;
-                          }
-                          Navigator.pop(context, todo);
-                        },
-                      ),
-                    ),
-                    Container(
-                      width: 5.0,
-                    ),
-                    Expanded(
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        child: Text(
-                          'Cancel',
-                          textScaleFactor: 1.5,
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                  ],
                 ),
               ),
             ],
           ),
-        ));
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          tooltip: 'Simpan',
+          onPressed: () {
+            if (todo == null) {
+              todo = ClassCatcher(
+                  titleController.text, descController.text);
+            } else {
+              todo.title = titleController.text;
+              todo.desc = descController.text;
+            }
+            Navigator.pop(context, todo);
+          },
+        ),
+      );
   }
 }
